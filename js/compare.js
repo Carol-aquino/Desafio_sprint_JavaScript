304ec6c852a05a3c4e673adcce133ef9b22a8c88
Carol-aquino <pinhocarolinapinhoaquino@gmail.com>
2026-05-13 13:30:26 -0300

    ajustando compare tabela

--- js/compare.js
+++ js/compare.js
@@ -7,6 +7,17 @@ class Car {
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
        
+        this.nome= nome;
+        this.preco= preco;
+        this.alturaCacamba= alturaCacamba;
+        this.alturaVeiculo= alturaVeiculo;
+        this.alturaSolo= alturaSolo;
+        this.capacidadeCarga= capacidadeCarga;
+        this.motor= motor;
+        this.potencia= potencia;
+        this.volumeCacamba= volumeCacamba;
+        this.roda= roda;
+        this.image= image;
     }
 } 
 
@@ -23,9 +34,22 @@ function SetCarToCompare(el, carClass) {
    
     if(carClass instanceof Car){       
         if(el.checked){
+
+            if(carArr.length >=2){
+                alert("Máximo de 2 carros.");
+                el.checked = false;
+                return;
+            }
+
+            carArr.push(carClass);
                 
             
         } else {
+            let position = GetCarArrPosition(carArr,carClass);
+
+            if(position >= 0) {
+                carArr.splice(position, 1);
+            }
           
         } 
     } else {
@@ -48,5 +72,45 @@ function HideCompare(){
 }
 
 function UpdateCompareTable() {
+    for(let i = 0; i < carArr.length; i++){
+
+        document.getElementById(`compare_image_${i}`).innerHTML =
+        `<img src="${carArr[i].image}" width="200">`;
+
+         document.getElementById(`compare_modelo_${i}`).innerHTML =
+         carArr[i].nome;
+
+          document.getElementById(`compare_preco_${i}`).innerHTML =
+        carArr[i].preco;
+
+        document.getElementById(`compare_motor_${i}`).innerHTML =
+        carArr[i].motor;
+
+        document.getElementById(`compare_potencia_${i}`).innerHTML =
+        carArr[i].potencia;
+
+         document.getElementById(`compare_alturaCacamba_${i}`).innerHTML =
+        carArr[i].alturaCacamba;
+
+         document.getElementById(`compare_alturaVeiculo_${i}`).innerHTML =
+        carArr[i].alturaVeiculo;
+
+         document.getElementById(`compare_alturaSolo_${i}`).innerHTML =
+        carArr[i].alturaSolo;
+
+         document.getElementById(`compare_capacidadedecarga_${i}`).innerHTML =
+        carArr[i].capacidadeCarga;
+
+         document.getElementById(`compare_volumecacamba_${i}`).innerHTML =
+        carArr[i].volumeCacamba;
+
+         document.getElementById(`compare_roda_${i}`).innerHTML =
+        carArr[i].roda;
+
+
+
+
+
+    }
     
 }
